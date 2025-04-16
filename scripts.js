d56e337e4f3082253a5f2223fd3b76940e140c11
eggeategg <45055954+eggeategg@users.noreply.github.com>
2025-04-15 18:16:49 -0700

    test

--- scripts.js
+++ scripts.js
@@ -23,58 +23,130 @@
  *
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+class imageCard {
+  constructor(title, image, location, description) {
+    this.title = title;
+    this.image = image;
+    this.location = location;
+    this.description = description;
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
+    
+  }
+  //here are a couple of methods to gather data I made in the begining
+  getName() {
+    return this.title;
+  }
+  getImage() {
+    return this.image;
+  }
+  getLoc() {
+    return this.location;
+  }
+  getDes() {
+    return this.description;
+  }
+}
+
+let imageCards = [
+  new imageCard(
+    "falling",
+    "./images/DSC_0172.jpg",
+    "Downtown, San Louis Obispo",
+    "This photo was taken as part of a project for a photography class I was taking at the time. The project was based around the idea that although man creates, nature allways finds a way to take back the ground."
+  ),
+  new imageCard(
+    "falling",
+    "./images/DSC_0226.jpg",
+    "SLO",
+    "description"
+  ),
+  new imageCard(
+    "falling",
+    "f",
+    "santa barbara",
+    "description"
+  ),
+  new imageCard(
+    "falling",
+    "f",
+    "santa maria",
+    "description"
+  )
+]
+
+
+
 // Your final submission should have much more data than this, and
 // you should use more than just an array of strings to store it all.
 
 // This function adds cards the page to display the data in the array
+
 function showCards() {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
+  for (let i = 0; i < imageCards.length; i++) {
+    let imageCard = imageCards[i];
 
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
+    // Extract car details
+    let title = imageCard.title;
+    let imageSrc = imageCard.image;
+    let location = imageCard.location;
+    let description = imageCard.description;
+   
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, title, imageSrc, location, description); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+//function showCards() {
+ // const cardContainer = document.getElementById("card-container");
+ // cardContainer.innerHTML = "";
+ // const templateCard = document.querySelector(".card");
+
+  //let imageURLs = [
+ //   "./ks0/images/DSC_0172.jpg",
+ //   "./ks0/images/DSC_0172.jpg",
+ //   "./images/east_los_high.jpg",
+  //];
+
+  //for (let i = 0; i < titles.length; i++) {
+   // let title = titles[i];
+
+    // This part of the code doesn't scale very well! After you add your
+    // own data, you'll need to do something totally different here.
+    
+    //let imageURL = imageURLs[i];
+
+    //const nextCard = templateCard.cloneNode(true); // Copy the template card
+   // editCardContent(nextCard, title, imageURL); // Edit title and image
+   // cardContainer.appendChild(nextCard); // Add new card to the container
+  //}
+//}
+
+function editCardContent(card, newTitle, newImageURL, location, description) {
   card.style.display = "block";
 
   const cardHeader = card.querySelector("h2");
   cardHeader.textContent = newTitle;
 
-  const cardImage = card.querySelector("img");
+  const cardImage = card.querySelector("#photo");
   cardImage.src = newImageURL;
   cardImage.alt = newTitle + " Poster";
 
+  const cardBullet1 = card.querySelector("#bullet1")
+  cardBullet1.textContent = location;
+  cardBullet1.alt = ":(";
+  cardBullet1.classList.add("whitespace-nowrap", "tracking-wide");
+
+  const cardBullet2 = card.querySelector("#bullet2")
+  cardBullet2.textContent = description;
+  cardBullet2.alt = ":(";
+
+  
   // You can use console.log to help you debug!
   // View the output by right clicking on your website,
   // select "Inspect", then click on the "Console" tab
